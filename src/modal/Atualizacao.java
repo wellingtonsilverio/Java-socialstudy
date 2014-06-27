1f8bcd06caf9d1635509b5cf16aec7d00501bfcd
Wellington A. A. Silverio <wellington.silverio@etec.sp.gov.br>
2014-06-27 02:29:59 -0300

    Temporidazador e aparecer janelas com as informações

--- src/modal/Atualizacao.java
+++ src/modal/Atualizacao.java
@@ -1,7 +1,12 @@
 package modal;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Timer;
@@ -15,9 +20,9 @@ import view.JFLogin;
 public class Atualizacao {
 	private int intIdUltAttResp, intIdUltAttPerg, intIdUltAttSeg;
 	private boolean blnBarulho;
-	private Timer tmrAtualizacao, tmrSumirCaixa;
-	long delay = (5)*1000;
-	long interval = (2*60*60)*1000;
+	private Timer tmrAtualizacao;
+	long delay;
+	long interval;
 	
 	private Opcoes objOpc;
 	
@@ -48,7 +53,7 @@ public class Atualizacao {
 			intIdUltAttSeg = 0;
 		}
 		
-		delay = (5)*1000;
+		delay = 1000;
 		interval = (objOpc.getIntTempoVeri()*60)*1000;
 		
 		blnBarulho = objOpc.isBlnFazerBarulho();
@@ -60,22 +65,75 @@ public class Atualizacao {
 			public void run() {
 				//select nos refesh, se tiver uma com tipo e id maior que o ultAtt vai abrir uma caixa lateral
 				try {
+					int y = 10;
+					
 					PreparedStatement stmt = null;
 			        String sql;
-			        sql = "SELECT * FROM refesh where usr_id = ?";
+			        sql = "SELECT * FROM refesh where user_id = ?";
 			        stmt = objOpc.getConn().prepareStatement(sql);
 			        stmt.setInt(1, objOpc.getUsrID());
 			        ResultSet rs = stmt.executeQuery();
 			        while(rs.next()){
-				        //usrNome = rs.getString("usr_nome");
+			        	delay = 1;
 			        	if(rs.getInt("att_tipo") == 3 && rs.getInt("att_id") > intIdUltAttPerg){
-			        		new JDJanela();
-			        	}
+			        		PreparedStatement stmt2 = null;
+					        String sql2;
+					        sql2 = "SELECT * FROM users where usr_id = ?";
+					        stmt2 = objOpc.getConn().prepareStatement(sql2);
+					        stmt2.setInt(1, rs.getInt("user_id"));
+					        ResultSet rs2 = stmt2.executeQuery();
+					        if(rs2.next()){
+					        	new JDJanela(
+				        				rs2.getString("usr_image"),
+				        				rs2.getString("usr_nome"),
+				        				rs.getString("att_desc"),
+				        				rs.getString("att_date"),
+				        				rs.getString("att_tipo"), y);
+					        	intIdUltAttPerg = rs.getInt("att_id");
+					        	salvar(intIdUltAttPerg, intIdUltAttResp, intIdUltAttSeg);
+					        	y += 210;
+					        }
+			        		
+			        	}else
 			        	if(rs.getInt("att_tipo") == 4 && rs.getInt("att_id") > intIdUltAttResp){
-			        		new JDJanela();
-			        	}
+			        		PreparedStatement stmt2 = null;
+					        String sql2;
+					        sql2 = "SELECT * FROM users where usr_id = ?";
+					        stmt2 = objOpc.getConn().prepareStatement(sql2);
+					        stmt2.setInt(1, rs.getInt("user_id"));
+					        ResultSet rs2 = stmt2.executeQuery();
+					        if(rs2.next()){
+					        	new JDJanela(
+				        				rs2.getString("usr_image"),
+				        				rs2.getString("usr_nome"),
+				        				rs.getString("att_desc"),
+				        				rs.getString("att_date"),
+				        				rs.getString("att_tipo"), y);
+					        	intIdUltAttResp = rs.getInt("att_id");
+					        	salvar(intIdUltAttPerg, intIdUltAttResp, intIdUltAttSeg);
+					        	y += 210;
+					        }
+			        	}else
 			        	if(rs.getInt("att_tipo") == 1 && rs.getInt("att_id") > intIdUltAttSeg){
-			        		new JDJanela();
+			        		PreparedStatement stmt2 = null;
+					        String sql2;
+					        sql2 = "SELECT * FROM users where usr_id = ?";
+					        stmt2 = objOpc.getConn().prepareStatement(sql2);
+					        stmt2.setInt(1, rs.getInt("user_id"));
+					        ResultSet rs2 = stmt2.executeQuery();
+					        if(rs2.next()){
+					        	new JDJanela(
+				        				rs2.getString("usr_image"),
+				        				rs2.getString("usr_nome"),
+				        				rs.getString("att_desc"),
+				        				rs.getString("att_date"),
+				        				rs.getString("att_tipo"), y);
+					        	intIdUltAttSeg = rs.getInt("att_id");
+					        	salvar(intIdUltAttPerg, intIdUltAttResp, intIdUltAttSeg);
+					        	y += 210;
+					        }
+			        	}else{
+			        		y = 10;
 			        	}
 			        }
 					//this.con.close();
@@ -99,6 +157,28 @@ public class Atualizacao {
 	}
 	public boolean verificarAtt(boolean blnNovaResp, boolean blnNovaPerg, boolean blnSeguidor){
 		return false;
+	}
+	public void salvar(int intIdUltAttPerg2, int intIdUltAttResp2, int intIdUltAttSeg2){
+		try {
+			FileWriter arqW;
+			arqW = new FileWriter("conf/.ultAtt.ss");
+	   		arqW.write(intIdUltAttPerg2+"\r\n");
+	   		arqW.write(intIdUltAttResp2+"\r\n");
+	   		arqW.write(intIdUltAttSeg2+"\r\n");
+	   		arqW.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			try {
+				File diretorio = new File("conf");
+				diretorio.mkdir();
+				File arqF = new File(diretorio, ".ultAtt.ss");
+				arqF.createNewFile();
+				salvar(intIdUltAttPerg2, intIdUltAttResp2, intIdUltAttSeg2);
+				
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+		}
 	}
 
 }
--- src/view/JDJanela.java
+++ src/view/JDJanela.java
@@ -4,8 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.Rectangle;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
@@ -15,6 +18,8 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -23,28 +28,40 @@ public class JDJanela extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	private Timer tmrSumirCaixa;
 	private float opacityForm = getOpacity();
+	private URL urlImage;
+	private BufferedImage image;
 
-	/**
-	 * Create the dialog.
-	 */
-	public JDJanela() {
+	
+	public JDJanela(String usrImage, String usrNome, String attDesc, String attDate, String attTipo, int y) {
 		setUndecorated(true);
 		setBounds(100, 100, 400, 200);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(null);
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		try {
+			try {
+				urlImage = new URL("http://localhost/rss/pags_logon/img/"+usrImage);
+			} catch (Exception e) {
+				// TODO: handle exception
+				urlImage = null;
+			}
+			image = ImageIO.read(urlImage);
+			image.getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+			JLabel labelImage = new JLabel(new ImageIcon(image));
+			labelImage.setBounds(10, 10, 100, 100);
+			contentPanel.add(labelImage);
+		} catch (Exception e) {
+			// TODO: handle exception
+			//JOptionPane.showMessageDialog(null, "Erro: "+e);
+		}
 		
-		JPanel panelImage = new JPanel();
-		panelImage.setBounds(10, 10, 100, 100);
-		contentPanel.add(panelImage);
-		
-		JLabel lblNome = new JLabel("");
+		JLabel lblNome = new JLabel(usrNome);
 		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNome.setBounds(120, 10, 270, 25);
 		contentPanel.add(lblNome);
 		
-		JLabel lblTexto = new JLabel("");
+		JLabel lblTexto = new JLabel(attDesc);
 		lblTexto.setBounds(120, 46, 270, 143);
 		contentPanel.add(lblTexto);
 		
@@ -64,7 +81,18 @@ public class JDJanela extends JDialog {
 		GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
 		Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
 		int x = ((int) rect.getMaxX() - getWidth())-10;
-		int y = 10;
+		if(y >= rect.getMaxY()-210){
+			double Rest = y - (rect.getMaxY());
+			x -= getWidth()+10;
+			y -= rect.getMaxY()-10;
+			y -= Rest;
+			if(y >= rect.getMaxY()-210){
+				double Rest2 = y - rect.getMaxY();
+				x -= (getWidth()+10)*2;
+				y -= rect.getMaxY()-10;
+				y -= Rest;
+			}
+		}
 		setLocation(x, y);
 		setVisible(true);
 		
